db31b32a51def4b05b35cc209b4c58c70a853f2c
Tiago Melo <tiagomelo013@gmail.com>
2021-08-23 09:29:06 -0300

    Projeto Pizzas finalizado

--- script.js
+++ script.js
@@ -129,8 +129,15 @@ function updateCart(){
     c('aside').classList.add('show');
     //zerar o carrinho de compras
     c('.cart').innerHTML = '';
+
+    //criar variavei para calculos antes do for
+    let subTotal = 0;
+    let desconto = 0;
+    let total = 0;
+
     for(let i in cart) {
       let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+      subTotal += pizzaItem.price * cart[i].qt;
       let cartItem = c('.models .cart--item').cloneNode(true);
      //Criar variavel para receber nome e tamanho da pizza
      
@@ -154,13 +161,32 @@ function updateCart(){
       cartItem.querySelector('img').src = pizzaItem.img;
       cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
       cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+      cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
 
+        if(cart[i].qt > 1){
+          cart[i].qt--;
+        }else{
+          cart.splice(i , 1);
+        }
+          updateCart();
         
+
+      });
+      cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+        cart[i].qt++;
+        updateCart()
+    });
 
       c('.cart').append(cartItem);
               
       
      }
+     desconto = subTotal * 0.1;
+     total = subTotal - desconto;
+
+     c('.subtotal span:last-child').innerHTML = `R$ ${subTotal.toFixed(2)}`;
+     c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+     c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
   } else {
     c('aside').classList.remove('show');
   }
